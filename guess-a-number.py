c639806d05a9a18b9b8d96d8a2cb972c3b55e815
Johanna Boberg <johanna.boberg@sofigate.com>
2022-05-13 23:28:41 +0300

    added a while loop for playing again

--- guess-a-number.py
+++ guess-a-number.py
@@ -2,40 +2,50 @@
 
 from random import randint
 
-#get a random number between 1 and 100
-luku = randint(1,100)
-#get the first guess from user
-arvaus = int(input("guess a number (1-100) "))
-arvausten_maara = 1
+play = True
 
-#check if the guess is close or not
-while arvaus != luku:
-	if arvaus < 0 or arvaus > 100:
-		print("OUT OF BOUNDS")
-		break
-	elif abs(luku - arvaus) <= 10:
-		print("WARM")
-		break
-	elif abs(luku - arvaus) > 10:
-		print("COLD")
-		break
-#get a new guess and check if it's closer or not
-while arvaus != luku:
-	uusi_arvaus = int(input("guess again "))
-	arvausten_maara += 1
-	while uusi_arvaus != luku:
-		if uusi_arvaus < 0 or uusi_arvaus > 100:
-			print("OUT OF BOUNDS")
-			break
-		elif abs(luku - arvaus) > abs(luku - uusi_arvaus):
-			print("WARMER")
-			break
-		elif abs(luku - arvaus) < abs(luku - uusi_arvaus):
-			print("COLDER")
-			break
-		elif abs(luku - arvaus) == abs(luku - uusi_arvaus):
-			print("same distance")
-			break
-	arvaus = uusi_arvaus
+while play == True:
+    #get a random number between 1 and 100
+    luku = randint(1,100)
+    #get the first guess from user
+    arvaus = int(input("guess a number (1-100) "))
+    arvausten_maara = 1
 
-print(f"you guessed it! The amount of guesses was {arvausten_maara}")
+    #check if the guess is close or not
+    while arvaus != luku:
+        if arvaus < 0 or arvaus > 100:
+            print("OUT OF BOUNDS")
+            break
+        elif abs(luku - arvaus) <= 10:
+            print("WARM")
+            break
+        elif abs(luku - arvaus) > 10:
+            print("COLD")
+            break
+    #get a new guess and check if it's closer or not
+    while arvaus != luku:
+        uusi_arvaus = int(input("guess again "))
+        arvausten_maara += 1
+        while uusi_arvaus != luku:
+            if uusi_arvaus < 0 or uusi_arvaus > 100:
+                print("OUT OF BOUNDS")
+                break
+            elif abs(luku - arvaus) > abs(luku - uusi_arvaus):
+                print("WARMER")
+                break
+            elif abs(luku - arvaus) < abs(luku - uusi_arvaus):
+                print("COLDER")
+                break
+            elif abs(luku - arvaus) == abs(luku - uusi_arvaus):
+                print("same distance")
+                break
+        arvaus = uusi_arvaus
+
+    print(f"you guessed it! The amount of guesses was {arvausten_maara}")
+    #ask if player wants to play again
+    again = input("Wanna play again? y/n")
+    if again.lower() == "y":
+        play = True
+    else:
+        print("Thanks for playing!")
+        play = False
